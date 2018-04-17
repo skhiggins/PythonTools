eb7ce6b38ed04e6676eec4984617842d2138328f
angelynaye <33856143+angelynaye@users.noreply.github.com>
2018-04-16 17:14:31 -0700

    Update plot.py

--- PlotData/plot.py
+++ PlotData/plot.py
@@ -11,7 +11,7 @@ allFiles = glob.glob(os.path.join(folder_base, "*.csv"))
 df_from_each_file = (pd.read_csv(f) for f in allFiles)
 concatenated_df  = pd.concat(df_from_each_file, ignore_index=True)
 colors = np.where(concatenated_df.POS > 1, 'r', 'g')# change ".POS" if the title of the column with data is named differently
-concatenated_df.plot(kind='scatter', x='id', y='POS', s=100, c=colors)# change value of x and y if the titles of the columns are named differently
+concatenated_df.plot(kind='scatter', x='id', y='POS', s=100, c=colors)# change x and y's values if the titles of the columns are named differently
 plt.xlabel( "Number of debit cards")
 plt.ylabel( "Number of businesses with POS terminal")
 plt.show()
